3920b92ebc92ebdf4c93334c75410c1e9c4e1b85
Alvin Ng <ngalvin93@gmail.com>
2019-09-25 23:47:11 -0500

    css update

--- public/main.js
+++ public/main.js
@@ -1,56 +1,53 @@
-const incrementButton = document.getElementById('increment')
-const decrementButton = document.getElementById('decrement')
-const resetButton = document.getElementById('reset')
-const output = document.getElementById('counter')
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const resetButton = document.getElementById('reset');
+const output = document.getElementById('counter');
 
-incrementButton.addEventListener('click', incrementCounter)
-decrementButton.addEventListener('click', decrementCounter)
-resetButton.addEventListener('click', resetCounter)
+incrementButton.addEventListener('mousedown', incrementCounter);
+decrementButton.addEventListener('mousedown', decrementCounter);
+resetButton.addEventListener('mousedown', resetCounter);
 
 function incrementCounter () {
-    console.log('increment')
     fetch('/increment', {
         method: 'POST'
     })
         .then(function (response) {
-            console.log(typeof response)
-            return response.json()
+            //console.log(typeof response)
+            return response.json();
         })
         .then(function (response) {
-            console.log(typeof response)
-            output.innerHTML = response
-            return
-        })
-}
+            //console.log(typeof response)
+            output.innerHTML = response;
+            return;
+        });
+};
 
 function decrementCounter () {
-    console.log('decrement')
-    console.log('increment')
     fetch('/decrement', {
         method: 'POST'
     })
         .then(function (response) {
-            console.log(typeof response)
-            return response.json()
+            //console.log(typeof response)
+            return response.json();
         })
         .then(function (response) {
-            console.log(typeof response)
-            output.innerHTML = response
-            return
-        })
-}
+            //console.log(typeof response)
+            output.innerHTML = response;
+            return;
+        });
+};
 
 function resetCounter () {
     fetch('/reset', {
         method: 'POST'
     })
         .then(function (response) {
-            console.log(typeof response)
-            return response.json()
+            //console.log(typeof response)
+            return response.json();
         })
         .then(function (response) {
-            console.log(typeof response)
-            output.innerHTML = response
-            return
-        })
-}
+            //console.log(typeof response)
+            output.innerHTML = response;
+            return;
+        });
+};
